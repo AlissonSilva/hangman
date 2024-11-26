a324fe052b5efceea4f141188a90c73ad1bc1c3d
Alisson Oliveira <alisson_sao@hotmail.com>
2024-11-26 13:04:51 -0300

    Create class GeneratorWordsIA

--- hangman_ai.py
+++ hangman_ai.py
@@ -79,3 +79,19 @@ class Force:
     @staticmethod
     def show(chances):
         return Force.step[chances]
+
+class GeneratorWordsIA:
+    def __init__(self):
+        self.generator = pipeline('text-generation', model='gpt-2')
+
+    def generate_words(self,categorie ):
+        """Gera palavras e dicas baseadas na categoria."""
+        prompt = f"Crie uma palavra e uma dica relacionadas à categoria: {categorie}."
+        answers = self.generator(prompt, max_length=50, num_return_sequences=1)
+        text_generate = answers[0]['generated_text']
+
+        try:
+            words, tip = text_generate.split(':',1)
+            return words.strip(), tip.strip()
+        except:
+            return 'WordGenerate', 'TipGenerate'
